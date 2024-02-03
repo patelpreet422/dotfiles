7f3b22001327b3d1aae00de3cc70fa2f2696ffbc
Preet Patel <patelpreet422@gmail.com>
2024-02-03 18:53:31 +0530

    refactor lsp configurations

--- nvim/lua/mason-config/init.lua
+++ nvim/lua/mason-config/init.lua
@@ -1,128 +1,30 @@
 -- [[ Configure mason ]]
 
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
+-- mason.setup must always preceed mason-lspconfig.setup
 require('mason').setup()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
-	-- clangd = {},
-	-- gopls = {},
-	-- pyright = {},
-	-- rust_analyzer = {},
-	-- tsserver = {},
-	-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-	lemminx = {},
+local lspconfig = require('lsp-config')
 
-	jsonls = {
-		filetypes = { 'json', 'jsonc' }
-	},
+-- list of nvim-lspconfig servers to install
+-- :lua P(require('mason-lspconfig').get_installed_servers())
+local servers = { 'gopls', 'lemminx', 'jsonls', 'lua_ls' }
 
-	lua_ls = {
-		filetypes = { 'lua' },
-		Lua = {
-			workspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-		},
-	},
 
-	jdtls = {
-		filetypes = { 'java' }
-	},
-}
-
--- This function gets run when an LSP connects to a particular buffer.
-local on_buffer_attach_to_lsp_server = function(_, bufnr)
-	-- NOTE: Remember that lua is a real programming language, and as such it is possible
-	-- to define small helper and utility functions so you don't have to repeat yourself
-	-- many times.
-	--
-	-- In this case, we create a function that lets us more easily define mappings specific
-	-- for LSP related items. It sets the mode, buffer and description for us each time.
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = 'LSP: ' .. desc
-		end
-
-		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-	end
-
-	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-	nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-	nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-	nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-	-- See `:help K` for why this keymap
-	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-	-- Lesser used LSP functionality
-	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-	nmap('<leader>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, '[W]orkspace [L]ist Folders')
-
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
-end
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local get_generic_lsp_capabilites = function()
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-	return capabilities
-end
-
+-- see :help mason-lspconfig-introduction
+-- this is approach is preferrable because this will automatically setup server installed 
+-- by mason as opposed to installing server first via mason and then setting up them 
+-- via nvim-lspconfig
 require('mason-lspconfig').setup {
-	ensure_installed = vim.tbl_keys(servers),
+	ensure_installed = servers,
+
 	handlers = {
-		function(server_name)
-			require('lspconfig')[server_name].setup {
-				capabilities = get_generic_lsp_capabilites(),
-				on_attach = on_buffer_attach_to_lsp_server,
-				settings = servers[server_name],
-				filetypes = (servers[server_name] or {}).filetypes,
-			}
-		end,
-		['jdtls'] = function()
-		end,
-		['lua_ls'] = function()
-			require('lspconfig').lua_ls.setup {
-				capabilities = get_generic_lsp_capabilites(),
-				on_attach = on_buffer_attach_to_lsp_server,
-				settings = {
-					Lua = {
-						runtime = {
-							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-							version = 'LuaJIT',
-						},
-						workspace = {
-							-- Make the server aware of Neovim runtime files
-							library = vim.api.nvim_get_runtime_file('', true),
-						},
-						-- Do not send telemetry data containing a randomized but unique identifier
-						telemetry = {
-							enable = false,
-						},
-					},
-				}
-			}
-		end,
+		-- see :help mason-lspconfig-setup
+		lspconfig.default_lsp_handler,
+
+		-- using nvim-jdtls and hence we don't want to setup jdtls using nvim-lspconfig
+		['jdtls'] = lspconfig.noop_handler,
+
+		-- customize lua_ls via nvim-lspconfig
+		['lua_ls'] = lspconfig.lua_lsp_handler,
 	},
 }
