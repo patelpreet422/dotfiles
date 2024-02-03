44b70c58945f2ec0883d6aeb8bbc9890661e43cf
Preet Patel <patelpreet422@gmail.com>
2024-02-03 20:40:39 +0530

    add completion close key binding

--- nvim/lua/nvim-cmp-config/init.lua
+++ nvim/lua/nvim-cmp-config/init.lua
@@ -19,6 +19,7 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.close(),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
